984fdd0200946cdab9d0210e37378e830ee95367
hiwad rashad <hiwadrashad112@hotmail.com>
2021-02-01 15:34:15 +0100

    Add project files.

---
+++ testeventargse/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace testeventargse
+{
+    public class testeventargs : EventArgs
+    {
+        public string teststring { get; set; }
+    }
+    public class Program
+    {
+        public EventHandler<testeventargs> eventname;
+        public event Action<object,EventArgs> something;
+        public static void Main(string[] args)
+        {
+            Program item = new Program();
+            item.executeeventargs();
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        public void executeeventargs()
+        {
+            something += (s, e) => { Console.WriteLine("test2"); };
+            testeventargs e = new testeventargs() { teststring = "testthis" };
+            eventname += testmethod;
+            eventname?.Invoke(this,e);
+        }
+
+        public static void testmethod(object sender, testeventargs e)
+        {
+            Console.WriteLine(e.teststring);
+        }
+    }
+
+    public class testclass
+    {
+        Program item = new Program();
+        public void main()
+        {
+            item.executeeventargs();
+        }
+    }
+}
